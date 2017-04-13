5532f29adb3bcfaacaf1d11b5a42397b8904b083
Luis Uzai <luisuzai13@gmail.com>
2017-04-13 20:59:30 -0300

    Palavras separadas

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
+class analise():
+    def analisador(self, text):
+        aux_string = ""
+        pass
+        for i in text:
+            aux_string = aux_string + i;
+            if(i == ';'or i=='+'or i== '-' or i == '*' or i== '/'or i== '=' or i== ',' or i== ';' or i== ':' or i== '<' or i== '>' or i== '('or i==')'):
+                # print(i)
+                aux_string = aux_string.replace(i, '')
+                print(aux_string)
+                vector_aux.append(aux_string)
+                vector_aux.append(i)
+                aux_string = ''
+
+            elif (i == ' '):
+                vector_aux.append(aux_string)
+                aux_string = ''
+
+pass
 
 text = (open('test.pas').read())
 
@@ -7,35 +26,15 @@ simbolos_especiais = [ '+', '-', '*', '/', '=', ',', ';', ':', '<', '>', '(', ')
 
 token = [ "AND", "ARRAY", "BEGIN", "CASE", "CONST", "DIV", "DO", "DOWNTO", "ELSE", "END", "FILE", "FOR", "FUNC", "GOTO", "IF", "IN", "LABEL", "MOD", "NOT", "OF", "OR", "PACKED", "PROCESS", "PROGRAM", "RECORD", "REPEAT", "SET", "THEN", "TO", "TYPE", "UNTIL", "VAR", "WHILE", "IF", "INTEGER", "REAL", "WRITELN", "READLN", "CHAR", "SHOWMSG" ]
 
-text_letters = list(text);
-aux_string = ""
-simbolos_especiais_aux = ''
+text_lines = text.splitlines()
+text_letters = []
+for z in text_lines:
+    text_letters.append(list(z))
+
 vector_aux = []
-
-#print(simbolos_especiais_aux == '')
-
-for j in simbolos_especiais:
-	
-	if(simbolos_especiais_aux == ''):
-		simbolos_especiais_aux = '"' + simbolos_especiais_aux  + j + '"'
-	
-	else:
-		simbolos_especiais_aux = simbolos_especiais_aux +   ' | ' + '"' + j + '"'
-
-	#print(simbolos_especiais_aux)
-
-
+yo = analise()
 for i in text_letters:
-	aux_string = aux_string + i;
-	
-	if(i == (simbolos_especiais_aux)):
-		print(i)
+    pass
+    yo.analisador(i)
 
-	elif i == ' ' | i == '\\n':
-		vector_aux.append(aux_string)
-		aux_string = ''
-		print(vector_aux)
-
-	else:
-		print('Não achou')
-
+print (vector_aux)
