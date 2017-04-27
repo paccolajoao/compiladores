c9f7f0209d14f875b4a5f89e1e5b8751fce52134
Luis <luis.bueno96@gmail.com>
2017-04-26 22:34:30 -0300

    Verificação de cadeia de caracteres e dois pontos e igual juntos adicionadas

--- lex.py
+++ lex.py
@@ -1,27 +1,56 @@
+#/usr/bin/python3
 class analise():
     def analisador(self, text):
         aux_string = ''
+        dois_pontos_igual = False
+        lendo_string = False
 
-        for i in text:
-            #print(i)
-            #vê se o i não é espaço e se não for, nem coloca
-            if(i != ' '):
-               aux_string = aux_string + i;
+        for i in range(len(text)):
+            #print(i,'  ',text[i])
+            # Verifica se está lendo string, e se estiver, adiciona a letra (incluindo espaço)
+            if(lendo_string):
+                aux_string = aux_string + text[i];
+            # Senão, vê se o text[i] não é espaço e se for, nem coloca
+            elif(text[i] != ' '):
+                   aux_string = aux_string + text[i];
+
+            if(dois_pontos_igual):
+                dois_pontos_igual = False
+                vector_aux.append(aux_string)
+                aux_string = ''
+
+            # verifica se o igual está ligado a um ponto
+            elif(text[i] == ':' and text[i+1] == '='):
+                dois_pontos_igual = True
+
             #compara para ver se é um limitador - pode ser melhorado!
-            if(aux_string in simbolos_especiais or i in simbolos_especiais):
+            elif(aux_string in simbolos_especiais or text[i] in simbolos_especiais):
                 #pega o simbolo e coloca vazio no lugar, para não salvar o simbolo junto com a palavra
-                aux_string = aux_string.replace(i, '')
+                aux_string = aux_string.replace(text[i], '')
                 #verifica se aux_string é diferente de vazio, se for, coloca no vetor
                 if(aux_string != ''):
                     vector_aux.append(aux_string)
                 #coloca o simbolo no vetor
-                vector_aux.append(i)
+                vector_aux.append(text[i])
                 #zera o aux_string, para pegar a proxima palavra
                 aux_string = ''
-            #se for espaço (que é um limitador e não temos que guardar)
-            elif (i == ' ' and aux_string != ''):
+            
+            # Se estiver lendo string e encontrar outro ', parar de ler string e adicionar cadeia na tabela
+            elif(lendo_string == True and text[i]=='\''):
+                lendo_string = False
                 vector_aux.append(aux_string)
                 aux_string = ''
+
+            # Se encontrar ' e nao estiver lendo string, começar a ler
+            elif(text[i]=='\'' and lendo_string == False):
+                lendo_string = True
+
+            #se for espaço (que é um limitador e não temos que guardar)
+            elif (text[i] == ' ' and aux_string != '' and lendo_string == False):
+                vector_aux.append(aux_string)
+                aux_string = ''
+
+
         #se a linha estiver sozinha (não tendo nenhum limitador) salva a palavra
         if(aux_string != ''):
             vector_aux.append(aux_string)
@@ -57,9 +86,9 @@ text = (open('test.pas').read())
 
 palavrasReservadas = [ "and", "array", "begin", "case", "const", "div", "do", "downto", "else", "end", "file", "for", "func", "goto", "if", "in", "label", "mod", "not", "of", "or", "packed", "process", "program", "record", "repeat", "set", "then", "to", "type", "until", "var", "while", "with", "integer", "real", "writeln", "readln", "char", "showmessage", "uses"]
 
-simbolos_especiais = [ '+', '-', '*', '/', '=', ',', ';', ':', '<', '>', '(', ')', '{', '}', '.', '|']
+simbolos_especiais = [ '+', '-', '*', '/', '=', ',', ';', ':', '<', '>', '(', ')', '{', '}', '.', '|',':=']
 
-token_especiais = [ "ADICAO", "SUBTRACAO", "MULTIPLICACAO", "DIVISAO", "IGUAL", "VIRGULA", "PONTO_VIRGULA", "DOIS_PONTOS", "MENOR", "MAIOR", "ABRE_PARENTESES", "FECHA_PARENTESES", "ABRE_CHAVES", "FECHA_CHAVES", "PONTO", "BARRA" ]
+token_especiais = [ "ADICAO", "SUBTRACAO", "MULTIPLICACAO", "DIVISAO", "IGUAL", "VIRGULA", "PONTO_VIRGULA", "DOIS_PONTOS", "MENOR", "MAIOR", "ABRE_PARENTESES", "FECHA_PARENTESES", "ABRE_CHAVES", "FECHA_CHAVES", "PONTO", "BARRA","DOIS_PONTOS_IGUAL"]
 
 vector_aux = []
 lex_table = []
